12377176ee03759bea1b12616fbdc53e0cf40a75
dieppa <hm.dieppa@gmail.com>
2018-10-03 11:52:08 +0200

    TableView added to ios project

---
+++ iosapp/src/main/java/com/martppa/java_clean_way/iosapp/views/CountryListViewController.java
@@ -0,0 +1,191 @@
+package com.martppa.java_clean_way.iosapp.views;
+
+
+import apple.NSObject;
+import apple.foundation.NSArray;
+import apple.foundation.NSBundle;
+import apple.foundation.NSCoder;
+import apple.foundation.NSMethodSignature;
+import apple.foundation.NSSet;
+import apple.uikit.UITableView;
+import apple.uikit.UIViewController;
+import org.moe.natj.c.ann.FunctionPtr;
+import org.moe.natj.general.NatJ;
+import org.moe.natj.general.Pointer;
+import org.moe.natj.general.ann.Generated;
+import org.moe.natj.general.ann.Mapped;
+import org.moe.natj.general.ann.MappedReturn;
+import org.moe.natj.general.ann.NInt;
+import org.moe.natj.general.ann.NUInt;
+import org.moe.natj.general.ann.Owned;
+import org.moe.natj.general.ann.RegisterOnStartup;
+import org.moe.natj.general.ann.Runtime;
+import org.moe.natj.general.ptr.VoidPtr;
+import org.moe.natj.objc.Class;
+import org.moe.natj.objc.ObjCRuntime;
+import org.moe.natj.objc.SEL;
+import org.moe.natj.objc.ann.ObjCClassBinding;
+import org.moe.natj.objc.ann.ObjCClassName;
+import org.moe.natj.objc.ann.Selector;
+import org.moe.natj.objc.map.ObjCObjectMapper;
+
+@Runtime(ObjCRuntime.class)
+@ObjCClassName("CountryListViewController")
+@RegisterOnStartup
+public class CountryListViewController extends UIViewController {
+	static {
+		NatJ.register();
+	}
+
+	@Generated
+	protected CountryListViewController(Pointer peer) {
+		super(peer);
+	}
+
+	@Override
+	public void viewDidLoad() {
+		super.viewDidLoad();
+	}
+
+	@Generated
+	@Selector("accessInstanceVariablesDirectly")
+	public static native boolean accessInstanceVariablesDirectly();
+
+	@Generated
+	@Owned
+	@Selector("alloc")
+	public static native CountryListViewController alloc();
+
+	@Generated
+	@Selector("allocWithZone:")
+	@MappedReturn(ObjCObjectMapper.class)
+	public static native Object allocWithZone(VoidPtr zone);
+
+	@Generated
+	@Selector("attemptRotationToDeviceOrientation")
+	public static native void attemptRotationToDeviceOrientation();
+
+	@Generated
+	@Selector("automaticallyNotifiesObserversForKey:")
+	public static native boolean automaticallyNotifiesObserversForKey(String key);
+
+	@Generated
+	@Selector("cancelPreviousPerformRequestsWithTarget:")
+	public static native void cancelPreviousPerformRequestsWithTarget(
+			@Mapped(ObjCObjectMapper.class) Object aTarget);
+
+	@Generated
+	@Selector("cancelPreviousPerformRequestsWithTarget:selector:object:")
+	public static native void cancelPreviousPerformRequestsWithTargetSelectorObject(
+			@Mapped(ObjCObjectMapper.class) Object aTarget, SEL aSelector,
+			@Mapped(ObjCObjectMapper.class) Object anArgument);
+
+	@Generated
+	@Selector("classFallbacksForKeyedArchiver")
+	public static native NSArray<String> classFallbacksForKeyedArchiver();
+
+	@Generated
+	@Selector("classForKeyedUnarchiver")
+	public static native Class classForKeyedUnarchiver();
+
+	@Generated
+	@Selector("clearTextInputContextIdentifier:")
+	public static native void clearTextInputContextIdentifier(String identifier);
+
+	@Generated
+	@Selector("countryTableView")
+	public native UITableView countryTableView();
+
+	@Generated
+	@Selector("debugDescription")
+	public static native String debugDescription_static();
+
+	@Generated
+	@Selector("description")
+	public static native String description_static();
+
+	@Generated
+	@Selector("hash")
+	@NUInt
+	public static native long hash_static();
+
+	@Generated
+	@Selector("init")
+	public native CountryListViewController init();
+
+	@Generated
+	@Selector("initWithCoder:")
+	public native CountryListViewController initWithCoder(NSCoder aDecoder);
+
+	@Generated
+	@Selector("initWithNibName:bundle:")
+	public native CountryListViewController initWithNibNameBundle(
+			String nibNameOrNil, NSBundle nibBundleOrNil);
+
+	@Generated
+	@Selector("instanceMethodForSelector:")
+	@FunctionPtr(name = "call_instanceMethodForSelector_ret")
+	public static native NSObject.Function_instanceMethodForSelector_ret instanceMethodForSelector(
+			SEL aSelector);
+
+	@Generated
+	@Selector("instanceMethodSignatureForSelector:")
+	public static native NSMethodSignature instanceMethodSignatureForSelector(
+			SEL aSelector);
+
+	@Generated
+	@Selector("instancesRespondToSelector:")
+	public static native boolean instancesRespondToSelector(SEL aSelector);
+
+	@Generated
+	@Selector("isSubclassOfClass:")
+	public static native boolean isSubclassOfClass(Class aClass);
+
+	@Generated
+	@Selector("keyPathsForValuesAffectingValueForKey:")
+	public static native NSSet<String> keyPathsForValuesAffectingValueForKey(
+			String key);
+
+	@Generated
+	@Owned
+	@Selector("new")
+	@MappedReturn(ObjCObjectMapper.class)
+	public static native Object new_objc();
+
+	@Generated
+	@Selector("resolveClassMethod:")
+	public static native boolean resolveClassMethod(SEL sel);
+
+	@Generated
+	@Selector("resolveInstanceMethod:")
+	public static native boolean resolveInstanceMethod(SEL sel);
+
+	@Generated
+	@Selector("setCountryTableView:")
+	public native void setCountryTableView_unsafe(UITableView value);
+
+	@Generated
+	public void setCountryTableView(UITableView value) {
+		Object __old = countryTableView();
+		if (value != null) {
+			org.moe.natj.objc.ObjCRuntime.associateObjCObject(this, value);
+		}
+		setCountryTableView_unsafe(value);
+		if (__old != null) {
+			org.moe.natj.objc.ObjCRuntime.dissociateObjCObject(this, __old);
+		}
+	}
+
+	@Generated
+	@Selector("setVersion:")
+	public static native void setVersion(@NInt long aVersion);
+
+	@Generated
+	@Selector("superclass")
+	public static native Class superclass_static();
+
+	@Generated
+	@Selector("version")
+	@NInt
+	public static native long version_static();
+}
